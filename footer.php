7379e16ad6d47e2e014ceca39b0314bf3d497516
BgitBB <61750923+BgitBB@users.noreply.github.com>
2022-01-12 19:04:27 +0300

    footer structure

---
+++ footer.php
@@ -0,0 +1,35 @@
+<footer class="bg-white container-100 border">
+        <div id="subscribe" class="w-40 text-center mx-auto mb-5">
+            <div class="p-5 text-secondary">
+                <p>Subscribe to Our Newsletter</p>
+            </div>
+            <div class="">
+                <form>
+                    <input class="form-control mb-2" type="email" placeholder="Enter your email here*">
+                    <button type="button" class="btn btn-dark w-100">Subscribe Now</button>
+                </form>
+            </div>
+        </div>
+        <div class="spacer-10"></div>
+        <div id="footer-footer" class="text-secondary">
+            <div class="d-flex justify-content-evenly">
+                <div class="">
+                    <p>© 2023 by Remo Art Gallery </p>
+                    <p>Proudly created with <a href="Wix.com">Wix.com</a></p>
+                </div>
+                <div class="">
+                    <p>500 Terry Francois Street</p>
+                    <p>San Francisco, CA 94158</p>
+                </div>
+                <div class=""><a href="Tel:123-456-7890">Tel: 123-456-7890</a></div>
+                <div class="">
+                    <a href="info@mysite.com">info@mysite.com</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    
+
+</body>
+
+</html>
