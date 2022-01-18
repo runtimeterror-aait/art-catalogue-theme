042d7a0a2f298617aa8d8d04ed7ee9599849d739
Michael Belete <50682242+michaelbelete@users.noreply.github.com>
2022-01-18 06:28:40 +0300

    created header and footer

--- footer.php
+++ footer.php
@@ -1,35 +1,21 @@
-<footer class="bg-white container-100 border">
-        <div id="subscribe" class="w-40 text-center mx-auto mb-5">
-            <div class="p-5 text-secondary">
-                <p>Subscribe to Our Newsletter</p>
-            </div>
-            <div class="">
-                <form>
-                    <input class="form-control mb-2" type="email" placeholder="Enter your email here*">
-                    <button type="button" class="btn btn-dark w-100">Subscribe Now</button>
-                </form>
-            </div>
-        </div>
-        <div class="spacer-10"></div>
-        <div id="footer-footer" class="text-secondary">
-            <div class="d-flex justify-content-evenly">
-                <div class="">
-                    <p>© 2023 by Remo Art Gallery </p>
-                    <p>Proudly created with <a href="Wix.com">Wix.com</a></p>
-                </div>
-                <div class="">
-                    <p>500 Terry Francois Street</p>
-                    <p>San Francisco, CA 94158</p>
-                </div>
-                <div class=""><a href="Tel:123-456-7890">Tel: 123-456-7890</a></div>
-                <div class="">
-                    <a href="info@mysite.com">info@mysite.com</a>
-                </div>
-            </div>
-        </div>
-    </footer>
-    
+<footer class="bg-white container-fluid border mt-5 py-4">
+  <div class="spacer-10"></div>
+  <div id="footer-footer" class="text-secondary">
+    <div class="d-flex justify-content-evenly">
+      <div class="">
+        <p>© 2023 <?php bloginfo('name'); ?> </p>
+      </div>
+      <div class="">
+        <p><?= bloginfo("description") ?></p>
+      </div>
+      <div class="">
+        <a href="info@mysite.com"><?= bloginfo('admin_email') ?></a>
+      </div>
+    </div>
+  </div>
+</footer>
 
+<?php wp_footer(); ?>
 </body>
 
 </html>
---
+++ header.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-transparent sticky-lg-top">
+    <div class="container-fluid px-3">
+        <a class="navbar-brand display-6 fw-bold" href="<?= get_home_url() ?>"><?php bloginfo('name'); ?></a>
+        <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+            <i class="bi bi-list" style="font-size: 2rem; color: black"></i>
+        </button>
+
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+            <div class="offcanvas-header">
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="mt-5">
+                <?php get_search_form(); ?> <?php wp_nav_menu(array(
+                                                'theme_location' => 'menu-1',
+                                                'menu_class' => 'sidebar row text-center mt-4'
+                                            )); ?>
+            </div>
+        </div>
+    </div>
+</nav>
